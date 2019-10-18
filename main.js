137e1f5b4a7a89fc301894f3608f94ae45258624
Vince Navarro <vincenavarro@gmail.com>
2019-10-18 10:04:21 -0400

    🚨 Autoprefixer ⚡️ Remove unnecessary classes 🎨 Prettier ♿️ Add alt text

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-'use strict'
+'use strict';
 
 const runSlideshow = () => {
+  const slides = document.querySelectorAll('#slideshow > img');
 
-	const slides = document.querySelectorAll('#slideshow > img');
+  let slideIndex = 0;
+  slides[0].style.opacity = 1;
 
-	let slideIndex = 0;
-	slides[0].style.opacity = 1;
+  const nextSlide = () => {
+    // If end of slide show, return to first slide.
+    const newSlide = slideIndex == slides.length - 1 ? 0 : slideIndex + 1;
+    slides.forEach(slide => (slide.style.opacity = 0));
+    slides[newSlide].style.opacity = 1;
+    slideIndex = newSlide;
+  };
 
-	const nextSlide = () => {
-		// If end of slide show, return to first slide.
-		const newSlide = slideIndex == slides.length - 1 ? 0 : slideIndex + 1;
-		slides.forEach(slide => slide.style.opacity = 0);
-		slides[newSlide].style.opacity = 1;
-		slideIndex = newSlide;
-	}
-
-	setInterval(() => {
-		nextSlide();
-	}, 4000);
-
-}
+  setInterval(() => {
+    nextSlide();
+  }, 4000);
+};
 
 document.addEventListener('DOMContentLoaded', () => runSlideshow(), false);
--- sw.js
+++ sw.js
@@ -1,40 +1,43 @@
-'use strict'
+'use strict';
 
 // If browser supports service worker, attempt to register it.
 if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('sw.js')
-			.then(response => console.log(`Service worker registered.`, response))
-			.catch(error => console.error(`Service worker registration failed.`, error));
-	});
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('sw.js')
+      .then(response => console.log(`Service worker registered.`, response))
+      .catch(error =>
+        console.error(`Service worker registration failed.`, error)
+      );
+  });
 }
 
 const cacheName = 'GardnerScreensaverCache';
 
 // Check if request is in cache. If found return it. If not attempt to cache it and then return it.
 self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => {
+        // A matching cache is found, return it.
+        if (response) return response;
 
-	event.respondWith(
-		caches.match(event.request)
-		.then(response => {
-
-			// A matching cache is found, return it.
-			if (response) return response;
-
-			// Not found in cache attempt to store it, then return it.
-			return fetch(event.request.clone()) //, { mode: 'no-cors' })
-				.then(response => {
-					caches.open(cacheName)
-						.then(cache => cache.put(event.request, response.clone()))
-						.catch(error => console.error(`Cache attempt failed:`, error));
-					return response.clone();
-				});
-		})
-		.catch(error => {
-			return new Response('Content not available.', {
-				status: 404,
-				statusText: error.message
-			});
-		})
-	);
+        // Not found in cache attempt to store it, then return it.
+        return fetch(event.request.clone()) //, { mode: 'no-cors' })
+          .then(response => {
+            caches
+              .open(cacheName)
+              .then(cache => cache.put(event.request, response.clone()))
+              .catch(error => console.error(`Cache attempt failed:`, error));
+            return response.clone();
+          });
+      })
+      .catch(error => {
+        return new Response('Content not available.', {
+          status: 404,
+          statusText: error.message
+        });
+      })
+  );
 });
