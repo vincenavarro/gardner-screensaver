862bb56a36ee6367e677d0ad743c0c5c873b8798
Vince Navarro <vincenavarro@gmail.com>
2019-10-23 14:09:06 -0400

    ♻️ Small refactor

--- main.js
+++ main.js
@@ -2,16 +2,14 @@
 
 const runSlideshow = () => {
   const slides = document.querySelectorAll('#slideshow > img');
-  slides[0].style.opacity = 1;
-
-  let slideIndex = 0;
+  const index = { value: 0 };
 
   const nextSlide = () => {
-    // If end of slide show, return to first slide.
-    const newSlide = slideIndex == slides.length - 1 ? 0 : slideIndex + 1;
     slides.forEach(slide => (slide.style.opacity = 0));
-    slides[newSlide].style.opacity = 1;
-    slideIndex = newSlide;
+    slides[index.value].style.opacity = 1;
+
+    // If end of slide show, return to first slide.
+    index.value == slides.length - 1 ? index.value = 0 : index.value++;
   };
 
   setInterval(() => {
