cc77cc806e632fac5a00228ae043ea42142fc7a9
Vince Navarro <vincenavarro@gmail.com>
2020-04-12 18:30:43 -0400

    ♻️ Remove unnecessary function

--- main.js
+++ main.js
@@ -4,16 +4,12 @@ const runSlideshow = () => {
   const slides = document.querySelectorAll('#slideshow > img');
   let index = 0;
 
-  const nextSlide = () => {
+  setInterval(() => {
     slides.forEach(slide => (slide.style.opacity = 0));
     slides[index].style.opacity = 1;
 
     // If end of slide show, return to first slide.
     index == slides.length - 1 ? index = 0 : index++;
-  };
-
-  setInterval(() => {
-    nextSlide();
   }, 5000);
 };
 
